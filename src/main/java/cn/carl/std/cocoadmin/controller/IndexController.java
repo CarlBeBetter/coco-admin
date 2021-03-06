793ac051a0a23696570e74f088610fcd90b11f6e
zhangtao <zhangtao@carl.local>
2021-03-07 01:09:08 +0800

    init coco-admin

---
+++ src/main/java/cn/carl/std/cocoadmin/controller/IndexController.java
@@ -0,0 +1,13 @@
+package cn.carl.std.cocoadmin.controller;
+
+/**
+ * @author zhangtao
+ * @Title: IndexControoler
+ * @Package: cn.carl.std.cocoadmin.controller
+ * @Description:
+ * @date 3/7/21 12:49 AM
+ */
+
+public class IndexController {
+
+}
